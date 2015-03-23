752e6f447f71986274682e2b1244c51bc3022957
Mikael Sennerholm <mikael@sennerholm.net>
2015-03-23 21:10:18 +0100

    First version wish start the daemon

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gocd-server
 Thoughtworks Go Server (from go.cd)
+
+
+
+Some scripts copied from: https://registry.hub.docker.com/u/gocd/gocd-server/
--- scripts/go-server-start.sh
+++ scripts/go-server-start.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+set -e
+
 . go-common-scripts.sh
+
+# Change /etc/go/cruise-config.xml before starting the server.
+# Setup auto registration for agents.
+# Only change if provided
+# AGENT_KEY="${AGENT_KEY:-123456789abcdef}"
+[[ -n "$AGENT_KEY" ]] && sed -i -e 's/agentAutoRegisterKey="[^"]*" *//' -e 's#\(<server\)\(.*artifactsdir.*\)#\1 agentAutoRegisterKey="'$AGENT_KEY'"\2#' /etc/go/cruise-config.xml
+
+# 
+
 
 show_msg "Starting Go Server ..."
 
-/sbin/setuser go /etc/init.d/go-server start &
+/etc/init.d/go-server start &
 
 wait_for_go_server
 
-# Setup auto registration for agents.
-AGENT_KEY="${AGENT_KEY:-123456789abcdef}"
-[[ -n "$AGENT_KEY" ]] && sed -i -e 's/agentAutoRegisterKey="[^"]*" *//' -e 's#\(<server\)\(.*artifactsdir.*\)#\1 agentAutoRegisterKey="'$AGENT_KEY'"\2#' /etc/go/cruise-config.xml
 
 cat <<EOF
 
